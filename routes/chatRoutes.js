088214b237ec635055d0197388c7026ceed7f402
Mohamed-Yassine-Youssef <yousssmed@gmail.com>
2022-09-26 19:53:57 +0100

    initial commit

---
+++ routes/chatRoutes.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const {
+  accessChat,
+  fetchChats,
+  createGroupChat,
+  renameGroup,
+  addToGroup,
+  removeFromGroup,
+} = require("../controllers/chatController");
+const { protect } = require("../middleware/authMiddleware");
+const router = express.Router();
+
+router.route("/").post(protect, accessChat).get(protect, fetchChats);
+router.post("/group", protect, createGroupChat);
+router.put("/rename", protect, renameGroup);
+router.put("/groupadd", protect, addToGroup);
+router.put("/groupremove", protect, removeFromGroup);
+
+module.exports = router;
